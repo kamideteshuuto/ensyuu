3f82b623189d60e1dd09d01f993b0aba80f9c26c
kamidate <s.kamidate.sys22@morijyobi.ac.jp>
2022-11-09 16:24:14 +0900

    内容の変更

--- Main.java
+++ Main.java
@@ -3,15 +3,18 @@ public class Main {
 	
 	public static void main(String[] args) {
 	 Scanner scan = new Scanner(System.in);
-		System.out.println("身長");
+	 for(int i=0; i<2;i++){
+                System.out.println("身長");
                 double height = scan.nextDouble();
                 System.out.println("体重");
                 double weight = scan.nextDouble();
 		double height2 = height / 100.0;
                 double BMI = weight / Math.pow(height,2);
                 System.out.println((double)Math.round(BMI * 100)/100);
-	 
+	 }
                 scan.close();
-	}	
-	}
+		
+	
+}
+}
 
